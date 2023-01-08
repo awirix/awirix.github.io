896bd73612746caae12c9d05875fedb1345f1bab
metafates <metafates@users.noreply.github.com>
2023-01-08 22:54:23 +0000

    deploy: 33a6373a1b4135495cabe786b743ca3c0668887c

---
+++ assets/guide_extensions_library.md.04b673c4.lean.js
@@ -0,0 +1,1 @@
+import{_ as a,c as s,o as e,d as o}from"./app.49da8a3a.js";const b=JSON.parse('{"title":"Library","description":"","frontmatter":{},"headers":[],"relativePath":"guide/extensions/library.md"}'),t={name:"guide/extensions/library.md"},n=o("",5),r=[n];function i(l,c,p,d,_,u){return e(),s("div",null,r)}const y=a(t,[["render",i]]);export{b as __pageData,y as default};
